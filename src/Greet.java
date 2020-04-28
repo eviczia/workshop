c3ad2cad55265b438b01dd5539e1d60bbfbdffbf
NemethJohanna <nemethkjohanna@gmail.com>
2020-04-28 21:52:27 +0200

    a5

--- src/Greet.java
+++ src/Greet.java
@@ -1,7 +1,7 @@
 public class Greet {
 
-    public void greet(String name){
-        System.out.println("Greetings!" + name);
+    public void greet(String greet, String name){
+        System.out.println(greet + name);
     }
 
 }
