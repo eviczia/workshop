17e3fb8300e916c5260c36ab2cb557bbc1224a18
NemethJohanna <nemethkjohanna@gmail.com>
2020-04-28 11:06:18 +0200

    a2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
 
-=======
-        System.out.println("Hello!");
->>>>>>> 00db98956bd9eba3ba4316bf2748337ab0157412
+
     }
 }
